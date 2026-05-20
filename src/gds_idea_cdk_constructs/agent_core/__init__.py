262101acc85ffd91a5a0d971cc6c3903626545e5
Jose Orjales <jose.orjales@digital.cabinet-office.gov.uk>
2026-05-20 18:07:11 +0100

    Made knowledge base config available more widely

--- src/gds_idea_cdk_constructs/agent_core/__init__.py
+++ src/gds_idea_cdk_constructs/agent_core/__init__.py
@@ -3,6 +3,7 @@ from .props import (
     AgentCoreProperties,
     BuiltInAgent,
     CustomAgent,
+    KnowledgeBaseConfig,
     MemoryConfig,
     ModelConfig,
 )
@@ -14,6 +15,7 @@ __all__ = [
     "BuiltInAgent",
     "CustomAgent",
     "DEFAULT_AGENT_CODE_DIR",
+    "KnowledgeBaseConfig",
     "MemoryConfig",
     "ModelConfig",
 ]
